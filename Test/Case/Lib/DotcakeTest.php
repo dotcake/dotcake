c6f59a624c53b91f903d539424844d5f40e8d6b9
k1LoW <k1lowxb@gmail.com>
2014-04-22 18:08:17 +0900

    use in_array()

--- Test/Case/Lib/DotcakeTest.php
+++ Test/Case/Lib/DotcakeTest.php
@@ -30,23 +30,23 @@ class DotcakeTestCase extends CakeTestCase {
     $this->assertEqual(realpath(APP . $result['cake']), realpath(CAKE_CORE_INCLUDE_PATH));
     $this->assertEqual(count($result['build_path']), 18);
 
-    $this->assertEqual($result['build_path']['models'][0], './Model/');
-    $this->assertEqual($result['build_path']['behaviors'][0], './Model/Behavior/');
-    $this->assertEqual($result['build_path']['datasources'][0], './Model/Datasource/');
-    $this->assertEqual($result['build_path']['databases'][0], './Model/Datasource/Database/');
-    $this->assertEqual($result['build_path']['sessions'][0], './Model/Datasource/Session/');
-    $this->assertEqual($result['build_path']['controllers'][0], './Controller/');
-    $this->assertEqual($result['build_path']['components'][0], './Controller/Component/');
-    $this->assertEqual($result['build_path']['auths'][0], './Controller/Component/Auth/');
-    $this->assertEqual($result['build_path']['acls'][0], './Controller/Component/Acl/');
-    $this->assertEqual($result['build_path']['views'][0], './View/');
-    $this->assertEqual($result['build_path']['helpers'][0], './View/Helper/');
-    $this->assertEqual($result['build_path']['consoles'][0], './Console/');
-    $this->assertEqual($result['build_path']['commands'][0], './Console/Command/');
-    $this->assertEqual($result['build_path']['tasks'][0], './Console/Command/Task/');
-    $this->assertEqual($result['build_path']['libs'][0], './Lib/');
-    $this->assertEqual($result['build_path']['locales'][0], './Locale/');
-    $this->assertEqual($result['build_path']['vendors'][0], './Vendor/');
-    $this->assertEqual($result['build_path']['plugins'][0], './Plugin/');
+    $this->assertTrue(in_array('./Model/', $result['build_path']['models']));
+    $this->assertTrue(in_array('./Model/Behavior/', $result['build_path']['behaviors']));
+    $this->assertTrue(in_array('./Model/Datasource/', $result['build_path']['datasources']));
+    $this->assertTrue(in_array('./Model/Datasource/Database/', $result['build_path']['databases']));
+    $this->assertTrue(in_array('./Model/Datasource/Session/', $result['build_path']['sessions']));
+    $this->assertTrue(in_array('./Controller/', $result['build_path']['controllers']));
+    $this->assertTrue(in_array('./Controller/Component/', $result['build_path']['components']));
+    $this->assertTrue(in_array('./Controller/Component/Auth/', $result['build_path']['auths']));
+    $this->assertTrue(in_array('./Controller/Component/Acl/', $result['build_path']['acls']));
+    $this->assertTrue(in_array('./View/', $result['build_path']['views']));
+    $this->assertTrue(in_array('./View/Helper/', $result['build_path']['helpers']));
+    $this->assertTrue(in_array('./Console/', $result['build_path']['consoles']));
+    $this->assertTrue(in_array('./Console/Command/', $result['build_path']['commands']));
+    $this->assertTrue(in_array('./Console/Command/Task/', $result['build_path']['tasks']));
+    $this->assertTrue(in_array('./Lib/', $result['build_path']['libs']));
+    $this->assertTrue(in_array('./Locale/', $result['build_path']['locales']));
+    $this->assertTrue(in_array('./Vendor/', $result['build_path']['vendors']));
+    $this->assertTrue(in_array('./Plugin/', $result['build_path']['plugins']));
   }
 }
