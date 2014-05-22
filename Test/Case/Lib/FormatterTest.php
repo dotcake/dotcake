9f0077bd03c32abab5e3bb7bbbf362f5762909b3
junichi11 <junichi0206@gmail.com>
2014-05-23 00:55:19 +0900

    Add FormatterTest

---
+++ Test/Case/Lib/FormatterTest.php
@@ -0,0 +1,103 @@
+<?php
+
+App::uses('Formatter', 'Dotcake.Lib');
+
+/**
+ * FormatterTestCase file
+ *
+ * @copyright     Copyright (c) dotcake organization. (https://github.com/dotcake)
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+class FormatterTestCase extends CakeTestCase {
+
+/**
+ * setUp
+ *
+ */
+	public function setUp() {
+	}
+
+/**
+ * tearDown
+ *
+ */
+	public function tearDown() {
+		unset($this->Formatter);
+	}
+
+/**
+ * testReformat
+ *
+ */
+	public function testReformat() {
+		$this->Formatter = new Formatter();
+		$text = <<< EOD
+{"cake":"..\/lib\/","build_path":{"models":[".\/Model\/"],"behaviors":[".\/Model\/Behavior\/"]}}
+EOD;
+		$result = $this->Formatter->reformat($text);
+		$expected = <<< EOD
+{
+	"cake":"..\/lib\/",
+	"build_path":{
+		"models":[
+			".\/Model\/"
+		],
+		"behaviors":[
+			".\/Model\/Behavior\/"
+		]
+	}
+}
+EOD;
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * testReformatWithWhitespace
+ *
+ */
+	public function testReformatWithWhitespace() {
+		$this->Formatter = new Formatter("    ");
+		$text = <<< EOD
+{"cake":"..\/lib\/","build_path":{"models":[".\/Model\/"],"behaviors":[".\/Model\/Behavior\/"]}}
+EOD;
+		$result = $this->Formatter->reformat($text);
+		$expected = <<< EOD
+{
+    "cake":"..\/lib\/",
+    "build_path":{
+        "models":[
+            ".\/Model\/"
+        ],
+        "behaviors":[
+            ".\/Model\/Behavior\/"
+        ]
+    }
+}
+EOD;
+		$this->assertEquals($expected, $result);
+	}
+
+/**
+ * testReformatQuotedBracesAndComma
+ *
+ */
+	public function testReformatQuotedBracesAndComma() {
+		$this->Formatter = new Formatter();
+		$text = <<< EOD
+{"cake":"}","build_path":{"models":[",["]}}
+EOD;
+		$result = $this->Formatter->reformat($text);
+		$expected = <<< EOD
+{
+	"cake":"}",
+	"build_path":{
+		"models":[
+			",["
+		]
+	}
+}
+EOD;
+		$this->assertEquals($expected, $result);
+	}
+
+}
